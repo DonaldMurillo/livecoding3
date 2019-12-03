67e0fb6040c166a492d01a10184f7bb29fc00044
Donald Murillo <megalion07@hotmail.com>
2019-12-03 18:59:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace livecoding3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+
+    public List<int> ZipArray(List<int> arrOne, List<int> arrTwo)
+    {
+        List<int> newArr = new List<int>();
+        for (int i = 0; i < arrOne.Count - 1; i++)
+        {
+            newArr.Add(arrOne[i]);
+
+        }
+        return newArr; 
+    }
+
+
+}
